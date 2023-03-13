5f9e8d508d53e68716c512fd996ff2c0e725cd25
Bruno Haetinger <brunohaetinger@gmail.com>
2023-03-12 22:20:48 -0300

    C++: Add note on factorialVoid

--- cpp/factorial-performance/factorial.cpp
+++ cpp/factorial-performance/factorial.cpp
@@ -11,7 +11,7 @@ mpz_class factorial(int n) {
 }
 
 // Due to type incopability between Python and "mpz_class", this returns nothing
-// So performance can still be measured
+// Note that the return value of factorial is a mpz_class object, which is not directly compatible with Python's native types. You may need to convert the result to a Python-compatible type using a library like PyGObject or PyGTK.
 void factorialVoid(int n) {
   mpz_class result = 1;
   for (int i = 2; i <= n; i++) {
