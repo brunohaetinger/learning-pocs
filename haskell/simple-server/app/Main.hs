db86f44597287836b9229c866cbb8fc2c71ef6f1
Bruno Haetinger <brunohaetinger@gmail.com>
2023-09-26 11:11:20 -0300

    Haskell: simplify server

--- haskell/simple-server/app/Main.hs
+++ haskell/simple-server/app/Main.hs
@@ -5,9 +5,7 @@ import Network.HTTP.Types (status200)
 import Network.Wai.Handler.Warp (run)
 import Data.UUID.V4 (nextRandom)
 import Data.UUID (toString)
-import Data.Text (Text)
-import qualified Data.Text.Encoding as TSE
-import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.ByteString.Lazy.Char8 (pack)
 
 main :: IO ()
 main = do
@@ -18,6 +16,6 @@ app :: Application
 app _ respond = do
     uuid <- nextRandom
     let uuidString = toString uuid
-        uuidText = TSE.decodeUtf8 $ BSL.toStrict $ BSL.pack uuidString
-        response = responseLBS status200 [("Content-Type", "text/plain")] (BSL.fromStrict $ TSE.encodeUtf8 uuidText)
+        uuidByteString = pack uuidString
+    let response = responseLBS status200 [("Content-Type", "text/plain")] uuidByteString
     respond response
