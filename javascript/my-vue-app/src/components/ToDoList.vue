9bfc0fe312b4d5da6e036429abe97c184fffd759
Bruno Haetinger <brunohaetinger@gmail.com>
2025-05-03 00:39:58 -0300

    Vue: started vue project with Todo List

---
+++ javascript/my-vue-app/src/components/ToDoList.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+const todos = ["Wake up", "Clean car"]
+</script>
+
+<template>
+  <h1>To Do List</h1>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    </ul>
+</template>
+
+<style scoped>
+.list {
+  color: #888;
+}
+</style>
