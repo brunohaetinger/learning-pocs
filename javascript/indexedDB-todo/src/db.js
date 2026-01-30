92b1015bd70f931e008ca0f2060dd9f76dfab127
Bruno Haetinger <brunohaetinger@gmail.com>
2026-01-29 23:44:50 -0300

    JS: indexedDB db openDB function

--- javascript/indexedDB-todo/src/db.js
+++ javascript/indexedDB-todo/src/db.js
@@ -3,4 +3,21 @@ const DB_VERSION = 1;
 const STORE_NAME = "clicks";
 
 function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    request.onerror = () => reject(request.error);
+
+    request.onsuccess = () => {
+      resolve(request.result);
+    }
+
+    request.onupgradedneeded = (event) => {
+      const db = event.target.result;
+
+      // create object store (table)
+      const store = db.createObjectStore(STORE_NAME, {
+        keyPath: "createdAt"
+      })
+    }
+  })
 }
