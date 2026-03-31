2ceb4bfc52cb6d23de0b64e44eb9a90c2c21d689
Bruno Haetinger <brunohaetinger@gmail.com>
2026-03-31 07:50:28 -0300

    OpenTUI: renderTodos function

--- typescript/opentui-todo/src/index.ts
+++ typescript/opentui-todo/src/index.ts
@@ -3,18 +3,40 @@ import {
   Box,
   createCliRenderer,
   Input,
+  InputRenderableEvents,
   Text,
   TextAttributes,
 } from "@opentui/core";
 
 const renderer = await createCliRenderer({ exitOnCtrlC: true });
-
+const todos: string[] = [];
 const titleInput = Input({
   placeholder: "New to-do item title",
   width: 50,
 });
 
+const todosContainer = Box({ flexDirection: "column" });
+
 titleInput.focus();
+
+const renderTodos = () => {
+  todosContainer.forEach(node => {
+    todosContainer.remove(node.id);
+  });
+
+  const todoNodes = todos.map(t => (Text({ content: t, attributes: TextAttributes.DIM })))
+
+  todoNodes.forEach(node => todosContainer.add(node));
+}
+
+titleInput.on(InputRenderableEvents.ENTER, (value: string) => {
+  todos.push(value);
+  titleInput.value = "";
+  renderTodos();
+})
+
+
+
 
 renderer.root.add(
   Box(
@@ -22,7 +44,9 @@ renderer.root.add(
     Box(
       { justifyContent: "center", alignItems: "flex-end" },
       ASCIIFont({ font: "tiny", text: "OpenTUI" }),
-      Text({ content: "New item:", attributes: TextAttributes.DIM }),
+      Text({ content: "Items:", attributes: TextAttributes.BOLD }),
+      todosContainer,
+      Text({ content: "New item:", attributes: TextAttributes.BOLD }),
       titleInput,
     ),
   ),
